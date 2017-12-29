beffce35cce0b9bdd6b9da360d845a95bc57b3ce
rabagi <programador1@telecomo.com.mx>
2017-12-28 18:09:29 -0600

    vue 10 transiciones!

---
+++ vue_10transiciones/js/main.js
@@ -0,0 +1,18 @@
+//vue.js
+
+new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensajes: 
+            {
+                transicion: 'Transiciones Css con vue.js', 
+                animacion: 'Animacion Css con vue.js',
+                animationCustom: 'Animaciones custom Css con vue.js',
+                entreElementos: 'Transiciones entre elementos con vue.js',
+        },
+          
+                      
+    },
+
+});

b8fa222497bb98e84f4c44080d930315f779a55e
rabagi <programador1@telecomo.com.mx>
2018-01-09 11:38:15 -0600

    vue 2.12 ejercicio de componentes!

---
+++ vue_2_12ejercicio_componente/js/main.js
@@ -0,0 +1,42 @@
+//vue.js
+Vue.component('usuarios',{
+    template:`#usuarios-template`,
+    mounted(){
+        axios.get('https://randomuser.me/api/?results=50')
+        .then((datos) => {
+            const listado = datos.data.results.map((usuario) => {
+                return {
+                    nombre: `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`,
+                    correoe: usuario.email,
+                    foto: usuario.picture.large,
+                }
+            });
+            this.usuarios = listado;
+        });
+    },
+    data(){
+        return {
+            usuarios: [],
+            busqueda: '',
+        }
+    },
+    computed:{
+        filtrarUsuarios(){
+            return this.usuarios.filter((usuario) => {
+                return usuario.nombre.includes(this.busqueda);
+            });
+        }
+    }
+});
+
+Vue.component('usuario', {
+    props:['datos'],
+    template: '#usuario-template',
+});
+
+new Vue({
+    el: 'main',
+    data:{
+       
+    }
+});

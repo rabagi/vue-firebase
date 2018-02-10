e618757bad9b09471b16b8a3c1e51c95e1641f35
Giovanny Ramirez Barrera [RaBaGi] <giovanny.rabagi@gmail.com>
2018-02-10 12:53:24 -0600

    seguimiendo 3.2 single component.

---
+++ 3_2single_file_comp/src/components/persona.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default{
+        mounted(){
+            axios.get('https://randomuser.me/api/')
+            .then((respuesta) =>{
+                this.persona = respuesta.data.results[0];
+            })
+        },
+        data(){
+            return {
+                persona: null,
+            }
+        },
+        
+
+    }
+</script>
+
+<style>
+</style>

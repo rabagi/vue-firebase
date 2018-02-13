d80e17c7cc92ccd8295891cc2f5c28169adea573
--Giovanny-- RBarrera <--Giovanny-- RBarrera>
2018-02-12 22:30:36 -0600

    se inicio vue  3.3 render  function!

---
+++ 3_3render_function/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <elemento tipo="section">
+      Hola mundo
+    </elemento>
+  </div>
+</template>
+
+<script>
+import elemento from './components/elemento.vue';
+export default {
+  components: {elemento}
+}
+</script>
+
+<style>
+</style>
---
+++ 3_3render_function/src/components/elemento.vue
@@ -0,0 +1,16 @@
+<template>
+    <section v-if="tipo='section'">
+        <slot></slot>
+    </section>
+</template>
+
+<script>
+    export default {
+        props : ['tipo']
+    }
+</script>
+
+<style>
+
+</style>
+

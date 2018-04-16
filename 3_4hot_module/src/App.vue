ce90d06ab97aab098471ebb741c36aee4a83f257
Giovanny Ramirez Barrera [RaBaGi] <giovanny.rabagi@gmail.com>
2018-04-16 13:10:07 -0500

    vue 3.4 hot module reoload

---
+++ 3_4hot_module/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <tareas></tareas>
+</div>  
+</template>
+
+<script>
+  import Tareas from './components/Tareas.vue';
+  export default {
+    components : {Tareas},
+  }
+</script>
+
+<style>
+
+</style>

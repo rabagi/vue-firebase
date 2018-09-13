9538bfd5b4d31f731d61be1a69765fee5e3919d7
Giovanny Ramirez Barrera [RaBaGi] <giovanny.rabagi@gmail.com>
2018-09-13 17:49:08 -0500

    vue 4.3 vuex state tree

---
+++ 4_3vuex_state_tree/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hola , soy {{nombre}} {{apellidos}} y me dedico a {{profesion}}</h1>
+</template>
+
+<script>
+  import {mapState} from 'vuex';
+  export default {
+    //primera forma
+    // computed: {
+    //   nombre(){
+    //     return this.$store.state.nombre;
+    //   }
+    // }
+    //segunda forma
+    // computed: mapState({
+    //   nombre: (state) => state.nombre,
+    //   apellidos: (state) => state.apellidos,
+    // })
+
+    computed: mapState(['nombre', 'apellidos', 'profesion', 'ciudad']),
+
+  }
+</script>

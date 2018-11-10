313c5fe475a974c14c716cac29fc121a1de88d34
Giovanny Ramirez Barrera [RaBaGi] <giovanny.rabagi@gmail.com>
2018-11-10 12:02:02 -0600

    4.8 vuex structure

---
+++ 4_8vuex_structure/src/components/Carro.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <h2>Carro de compra</h2>
+        <h3>Total de compra: {{ totalCompra.toFixed(2) }} $</h3>
+        <ul>
+            <li v-for="(producto, indice) in carro">
+                {{ producto.nombre }}
+                <button @click="eliminarProducto(indice)">x</button>
+            </li>
+        </ul>
+    </section>
+</template>
+<script>
+    import {mapState, mapGetters, mapMutations} from 'vuex';
+    export default {
+        computed:{
+            ...mapState(['carro']),
+            ...mapGetters(['totalCompra']),
+        },
+        methods: mapMutations(['eliminarProducto'])
+    }
+</script>

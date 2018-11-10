f404b57fbe1a359c602e3bc481945001f3a270eb
Giovanny Ramirez Barrera [RaBaGi] <giovanny.rabagi@gmail.com>
2018-11-10 12:06:52 -0600

    4.8 vuex estructure fix

---
+++ 4_7vuex_modules/src/store/getters.js
@@ -0,0 +1,5 @@
+export const getters = {
+    
+    totalCompra: (state) => state.carro.reduce((total, producto) => total + producto.precio, 0),
+
+};
--- 4_7vuex_modules/src/store/store.js
+++ 4_7vuex_modules/src/store/store.js
@@ -1,27 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import productos from './modules/productos';
+import {getters} from './getters';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        // productos:[
-        //     {nombre: 'stream link', precio:50},
-        //     {nombre: 'steam controller', precio:59},
-        //     {nombre: 'axiom link', precio:17}
-        // ],
         carro: [],
     },
 
-    getters: {
-        totalCompra: (state) => state.carro.reduce((total, producto) => total + producto.precio, 0),
-    },
+    getters: getters,
 
-    mutations: {
-        //anadirProducto: (state, producto) => state.productos.unshift(producto),
-        comprarProducto: (state, indice) => state.carro.unshift(state.productos[indice]),
-        eliminarProducto: (state, indice) => state.carro.splice(indice, 1),
-    },
+    mutations:  mutations,
 
     modules: {
         productos,

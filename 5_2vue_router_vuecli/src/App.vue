f3a5889af825eae43c29bf16e660f6e0f69406f0
rabagi <programador1@telecomo.com.mx>
2019-05-21 17:54:11 -0500

    Vue 5.2 Vue router y vueCLi

---
+++ 5_2vue_router_vuecli/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>Rutas con vue</h1>
+        <router-link to="/">Portada</router-link>
+        <router-link to="/contacto">Portada</router-link>
+    <hr>
+       
+        <router-view></router-view>
+  </section>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>

8ac82695d8e9591f46ca8bce65203dce3250da8d
rabagi <programador1@telecomo.com.mx>
2019-06-27 18:24:34 -0500

    Vue router active links

---
+++ 5_3vue_router_activelinks/src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/home.vue';
+import Contacto from './components/contacto.vue';
+
+export const routes = [
+    {path: '/', component: Home},
+    {path: '/contacto', component: Contacto},
+    
+];
+

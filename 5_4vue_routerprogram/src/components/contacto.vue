03d049255101b7e60e19fae8b88fe637ac749ee3
rabagi <programador1@telecomo.com.mx>
2019-06-28 17:44:07 -0500

    Vue 5.4 Router programmatic

---
+++ 5_4vue_routerprogram/src/components/contacto.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+    <h2>Contacto</h2>
+    <button @click="volverHome()">regresar a home</button>
+    </section>
+</template>
+
+<script>
+    export default{
+        created(){
+            setTimeout(()=> this.$router.push('/'), 1000)
+        },
+        methods:{
+            volverHome(){
+                this.$router.push('/', ()=> console.log('ruta cambiada')); 
+            }
+        }
+    }
+</script>

b84e90e8c1e9a3b080a76e9ad09a1a594b41f7a2
rabagi <programador1@telecomo.com.mx>
2019-07-26 13:07:32 -0500

    Vue 5.9 Router named rotes

---
+++ 5_9vue_router_named/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <h1>Home</h1>
+        <!-- <router-link :to="'/equipo/'+id">Equipo</router-link> -->
+        <router-link :to="{ name: 'equipo', params: { id: id }}">Equipo</router-link>
+    </main>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                id: 'rabagi'
+            }
+        }
+    }
+</script>

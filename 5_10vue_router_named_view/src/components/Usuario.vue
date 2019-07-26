cb0810678c3caa91c7f49e59c10694b1d9f27817
rabagi <programador1@telecomo.com.mx>
2019-07-26 14:17:12 -0500

    Vue 5.10 router named views

---
+++ 5_10vue_router_named_view/src/components/Usuario.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <h1>El identificador es: {{ $route.params.id }}</h1>
+        <h2>Giovanny Ramírez Barrera</h2>
+        <h5>rabagi@outlook.com</h5>
+        <!-- <router-link :to="'/equipo/' + $route.params.id +'/fotos'">Fotos</router-link>
+        <router-link :to="'/equipo/' + $route.params.id +'/bio'">Bio</router-link> -->
+
+         <!-- <router-link :to="{ name: 'fotos', params: { id: $route.params.id }}">Fotos</router-link>
+        <router-link :to="{ name: 'bio', params: { id: $route.params.id }}">Bio</router-link>
+        <router-view></router-view> -->
+
+    </main>
+</template>

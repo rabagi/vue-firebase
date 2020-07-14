f233b8baab8e9039ea6abe884d44029b1931c267
rabagi <programador1@telecomo.com.mx>
2020-07-14 11:52:18 -0500

    Vue 5_12 vue routing pase

---
+++ 5_12vue_router_passing_props/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h1>Home</h1>
+    <router-link :to="{ name: 'equipo', params: { id: id} }">
+      Equipo
+    </router-link>
+    <router-link :to="{ name: 'contacto'}">
+      Contacto
+    </router-link>
+  </section>
+</template>
+
+<script>
+  export default{
+      data(){
+          return {
+              id: 'rabagi'
+          }
+      }
+  }
+</script>

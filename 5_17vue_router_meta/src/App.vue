619b40c8c94308f223a1e741b320dddc617ad08f
rabagi <programador1@telecomo.com.mx>
2020-07-22 17:34:57 -0500

    vue 5.17 router meta fields

---
+++ 5_17vue_router_meta/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h2>Welcome</h2>
+    <section>
+      <button @click="cambiar">Cambiar auth</button>
+      <router-view></router-view>
+    </section>    
+  </div>
+</template>
+
+<script>
+import{mapMutations} from 'vuex';
+export default {
+  methods: mapMutations(['cambiar'])
+}
+</script>
+
+<style>
+
+</style>

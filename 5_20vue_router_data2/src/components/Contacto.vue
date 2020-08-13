d3ec21081698bb110b023a8f68b5c9704b87a1b1
rabagi <programador1@telecomo.com.mx>
2020-08-13 17:26:41 -0500

    vue 5.20 vue router data fetch 2

---
+++ 5_20vue_router_data2/src/components/Contacto.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <h2>Contacto</h2>
+        <p>Contactarme ... </p>
+        <h5>{{ newsletter }}</h5>
+        <pre>{{ numero }}</pre>
+        <template v-if="newsletter">
+            <form action="">
+                <h3>Newsletter</h3>
+                <label for="correoe">Email</label>
+                <input type="email" placeholder="Correo electronico" id="correoe">
+            </form>
+        </template>
+    </section>
+</template>
+<script>
+export default {
+    props: ['newsletter'],
+    data(){
+        return {
+            numero: null,
+        }
+    },
+    beforeRouteEnter: ((to, from, next)=> {
+        console.log('antes de entrar...');
+        setTimeout( (vm)=>{
+            next((vm) => {
+                vm.numero = Math.floor(Math.random() * 1000);
+            });
+                        
+        }, 2000);
+    }),
+}
+</script>
---
+++ 5_20vue_router_data2/src/components/Usuario.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <h1>El identificador es: {{ $route.params.id }}</h1>
+        <h2>Giovanny Ramírez Barrera</h2>
+        <h5>rabagi@outlook.com</h5>
+        <pre v-text="username"></pre>
+    </section>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                username: null
+            }
+        },
+        created(){
+            this.obtenerUSername();
+        },
+        watch:{
+            '$route': 'obtenerUsername'
+        },
+        methods:{
+            obtenerUSername(){
+                setTimeout(()=>{
+                    this.username = this.$route.params.id + Math.floor(Math.random()*1000);
+                }, 2000);
+                
+            }
+        },
+        beforeRouteEnter: ((to, from, next) => {
+            console.info('Antes de entrar');
+            next((vm) => console.log(vm));
+        }),
+        beforeRouteUpdate: ((to, from, next) => {
+            console.info('Antes de cambiar  la ruta sobre el mismo componente');
+            next();
+        }),
+        beforeRouteLeave: ((to, from, next) => {
+            console.info('Antes de salir');
+            next();
+        }),
+    }
+</script>

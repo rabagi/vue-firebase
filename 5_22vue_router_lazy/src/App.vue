ddc9bb2ac321fb08dc615b0b49d2ae547b7dd038
rabagi <programador1@telecomo.com.mx>
2020-08-19 16:39:57 -0500

    5.22 vue router lazy

---
+++ 5_22vue_router_lazy/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+      <router-link to="/">Innfo</router-link>
+      <router-link :to="{ path: '/bio', hash: '#parrafo'}">Bio</router-link>
+      <hr>
+      <router-view></router-view>
+  </section>
+</template>
+ 
---
+++ 5_22vue_router_lazy/src/components/Info.vue
@@ -0,0 +1,25 @@
+<template>
+   <div id="app">
+       <img src="../assets/logo.png">
+       <h1></h1>
+       <h2>Essential lnks</h2>
+       <ul>
+           <li><a href="google.com">Guugle</a></li>
+           <li><a href="google.com">Guugle 2</a></li>
+           <li><a href="google.com">Guugle 3</a></li>
+           <li><a href="google.com">Guugle 4</a></li>
+       </ul>
+       <h2>Ecosystems</h2>
+       <ul>
+           <li><a href="google.com">Guugle</a></li>
+           <li><a href="google.com">Guugle 2</a></li>
+           <li><a href="google.com">Guugle 3</a></li>
+           <li><a href="google.com">Guugle 4</a></li>
+       </ul>
+   </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
